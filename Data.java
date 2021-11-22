cb84591f67625a17cf439bea95f1efb2436bbcbe
rahulthambi10@gmail.com <rahulthambi10@gmail.com>
2021-11-22 16:43:05 +0530

    New One

---
+++ Data.java
@@ -0,0 +1,26 @@
+public class Data {
+
+	public static void main(String[] args) {
+		int myNumber = 22;
+		short myShort = 847;
+		long myLong = 9797;
+		
+		double myDouble = 34.5645;
+		float myFloat =343.5f;
+		
+		char myChar = 'y';
+		boolean myBoolean = true;
+		
+		byte myByte = 127;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+	}
+
+}
