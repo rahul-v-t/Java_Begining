18b6bb1eac247bf09cc0a9212ec71d7cee51df7d
rahulthambi10@gmail.com <rahulthambi10@gmail.com>
2021-11-22 17:07:47 +0530

    Updated

---
+++ Strings.java
@@ -0,0 +1,18 @@
+public class Strings {
+	public static void main(String[] args) {
+		int myNum = 20;
+		
+		String text = "Hello";
+		
+		String blank = " ";
+		
+		String name = "Rahul";
+		
+		String greeting = text + blank + name ;		
+		
+		System.out.println(greeting);
+		
+		System.out.println("Hello" + " " + name);
+		System.out.println("My Number Is " + myNum);
+	}
+}
